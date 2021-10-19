841e21d52058ec4b7e1d78a5b6856a9464d33281
lucamagenes <lucamgns33@gmail.com>
2021-10-19 18:34:50 +0200

    creata function checkPalindromi function checkPalindromi non funzionante

--- palidroma/assets/js/main.js
+++ palidroma/assets/js/main.js
@@ -16,3 +16,33 @@
 
 */
 
+const userWord = prompt('Scrivi una parola');
+console.log(userWord);
+
+const letters = userWord.split('');
+console.log(letters);
+
+const reverseLetters = letters.reverse();
+console.log(reverseLetters);
+
+
+
+function checkPalindromi(lettere) {
+
+    let a = lettere;
+    let b = lettere.reverse();
+    if (a === b) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+
+if (checkPalindromi(letters)) {
+    console.log('è una parola palindroma');
+
+} else {
+    console.log('non è una parola palindroma');
+}
