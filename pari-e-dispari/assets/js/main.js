a7a73b9b01e3236977799ac623dad1c3d17babcd
lucamagenes <lucamgns33@gmail.com>
2021-10-19 14:52:51 +0200

    commit iniziale creazione dei file di lavoro creazione della struttura base di pari-e-dispari

---
+++ pari-e-dispari/assets/js/main.js
@@ -0,0 +1,28 @@
+//l'utente sceglie PARI o DISPARI
+//l'utente sceglie un numero da 1 a 5
+
+//generiamo un numero RANDOM da 1 a 5 per il pc
+
+//sommiamo i due numeri
+
+//stabilire se la somma dei due numeri è pari o dispari
+
+//stabilire chi ha vinto
+
+
+/* strumenti
+
+- prompt (pari o dispari)
+- parseInt (prompt () )
+
+- math.floor (math.random () )
+
+- a + b = c
+
+- if
+    - c % 2 == 0
+
+- console.log (vincitore)
+
+*/
+
