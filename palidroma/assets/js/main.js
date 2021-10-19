b0eebcd32f8be92acdc1c895642b94acff6dbe5f
lucamagenes <lucamgns33@gmail.com>
2021-10-20 00:08:36 +0200

    esercizio palindromi funzione non corretta e funzionante

--- palidroma/assets/js/main.js
+++ palidroma/assets/js/main.js
@@ -16,33 +16,41 @@
 
 */
 
-const userWord = prompt('Scrivi una parola');
+let userWord = prompt('Scrivi una parola');
 console.log(userWord);
 
 const letters = userWord.split('');
 console.log(letters);
 
-const reverseLetters = letters.reverse();
+
+
+function checkPalindromi(parola) {
+
+    return parola === parola.reverse();
+
+}
+
+const reverseLetters = checkPalindromi(letters)
 console.log(reverseLetters);
 
 
 
-function checkPalindromi(lettere) {
+/*
+var checkWord = letters.length == reverseLetters.length && letters.every(function (element, index) {
+    return element === reverseLetters[index];
+});
 
-    let a = lettere;
-    let b = lettere.reverse();
-    if (a === b) {
-        return true
-    } else {
-        return false
-    }
-
-}
+console.log(checkWord);
 
 
-if (checkPalindromi(letters)) {
-    console.log('è una parola palindroma');
+var array1 = ['c', 'i', 'a', 'o'];
+console.log(array1);
+var array2 = ['c', 'a', 'i', 'o'];
+console.log(array2);
 
-} else {
-    console.log('non è una parola palindroma');
-}
+var is_same = array1.length == array2.length && array1.every(function (element, index) {
+    return element === array2[index];
+});
+
+console.log(is_same);
+*/
