fb333dc46124ece5cf7436bffd75dafdde292ffe
lucamagenes <lucamgns33@gmail.com>
2021-10-19 16:44:39 +0200

    creata struttura di base es palindromi

---
+++ palidroma/assets/js/main.js
@@ -0,0 +1,18 @@
+//Chiedere all'utente di inserire una parola
+
+//creare una funzione
+
+//verificare se la parola è palindroma
+
+/* strumenti
+
+- prompt
+
+- function
+
+- array
+
+- if / else
+
+*/
+
